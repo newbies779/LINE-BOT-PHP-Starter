056eb77906976546a81b657be9c5b0cc5ba0d937
ANUPONG_KNOT_PC\Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-26 15:08:10 +0700

    qwe

--- bot.php
+++ bot.php
@@ -2,3 +2,4 @@
 $httpClient = new vendor\linecorp\line-bot-sdk\src\LINEBot\HTTPClient\CurlHTTPClient('7xGXQTgIeNebLt9q7UtuY8TdPI6T8eAx1WxsHp5i38siySOMQrZ5wyc0A1xUFpLQ9s7DErBfFTdmeM3Gw73Clgxb4wN5uWpfGNgU68VkOFZMBu9ss2axWxrwMLfVR1WSBl7r02mgEKMO2pA59QVgKwdB04t89/1O/w1cDnyilFU=');
 $bot = new vendor\linecorp\line-bot-sdk\src\LINEBot($httpClient, ['channelSecret' => '	
 d2a932fbcb794319177f57f78cdc9493']);
+echo 'something';
