2ccac4b448a9056ef09eab191a25e0e07dbcff70
ANUPONG_KNOT_PC\Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-26 16:26:01 +0700

    eqweqwe

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $path = __DIR__ . '/vendor/autoload.php';
 require_once $path;
-echo 'required';
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('7xGXQTgIeNebLt9q7UtuY8TdPI6T8eAx1WxsHp5i38siySOMQrZ5wyc0A1xUFpLQ9s7DErBfFTdmeM3Gw73Clgxb4wN5uWpfGNgU68VkOFZMBu9ss2axWxrwMLfVR1WSBl7r02mgEKMO2pA59QVgKwdB04t89/1O/w1cDnyilFU=');
-echo 1;
+
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd2a932fbcb794319177f57f78cdc9493']);
-echo 2;
+
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('u986352e98748ad741ecb94468ed2f7e0', $textMessageBuilder);
+$response = $bot->pushMessage(['u986352e98748ad741ecb94468ed2f7e0'], $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 // 
