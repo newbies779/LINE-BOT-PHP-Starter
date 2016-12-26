d971524cf6bb0dafb6185f377366de45086c87a1
ANUPONG_KNOT_PC\Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-26 16:07:05 +0700

    ewqeqw

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
-			var_dump($event);
+			echo $event;
 			// // Make a POST Request to Messaging API to reply to sender
 			// $url = 'https://api.line.me/v2/bot/message/reply';
 			// $data = [
