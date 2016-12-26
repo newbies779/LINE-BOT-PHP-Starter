56c1eaac3cda814c1aaa4736ced21e51909a5753
ANUPONG_KNOT_PC\Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-26 16:13:34 +0700

    3213213

--- bot.php
+++ bot.php
@@ -6,10 +6,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 echo 'running';
-echo $event;
-
-
-echo $event['userId'];
+echo $events['events'];
 echo '1\r\n';
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
