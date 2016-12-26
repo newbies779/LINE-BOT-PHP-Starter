e14c217a3db2b836f414bf4e9d5e3b094a44bc34
ANUPONG_KNOT_PC\Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-26 15:02:58 +0700

    te

--- bot.php
+++ bot.php
@@ -1,2 +1,4 @@
 <?php 
-echo "I'm a bot";
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('7xGXQTgIeNebLt9q7UtuY8TdPI6T8eAx1WxsHp5i38siySOMQrZ5wyc0A1xUFpLQ9s7DErBfFTdmeM3Gw73Clgxb4wN5uWpfGNgU68VkOFZMBu9ss2axWxrwMLfVR1WSBl7r02mgEKMO2pA59QVgKwdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '	
+d2a932fbcb794319177f57f78cdc9493']);
