c8d3797a067b8c0c6fd3fb0995525cc78cce6771
ANUPONG_KNOT_PC\Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-26 15:58:29 +0700

    ewqeqw

--- index.php
+++ index.php
@@ -1,27 +1,7 @@
 <?php
-namespace LINE\LINEBot;
-use LINE\LINEBot;
-use LINE\LINEBot\Constant\HTTPHeader;
-use LINE\LINEBot\Event\BeaconDetectionEvent;
-use LINE\LINEBot\Event\FollowEvent;
-use LINE\LINEBot\Event\JoinEvent;
-use LINE\LINEBot\Event\LeaveEvent;
-use LINE\LINEBot\Event\MessageEvent;
-use LINE\LINEBot\Event\MessageEvent\AudioMessage;
-use LINE\LINEBot\Event\MessageEvent\ImageMessage;
-use LINE\LINEBot\Event\MessageEvent\LocationMessage;
-use LINE\LINEBot\Event\MessageEvent\StickerMessage;
-use LINE\LINEBot\Event\MessageEvent\TextMessage;
-use LINE\LINEBot\Event\MessageEvent\VideoMessage;
-use LINE\LINEBot\Event\PostbackEvent;
-use LINE\LINEBot\Event\UnfollowEvent;
-use LINE\LINEBot\Exception\InvalidEventRequestException;
-use LINE\LINEBot\Exception\InvalidSignatureException;
-use LINE\LINEBot\Exception\UnknownEventTypeException;
-use LINE\LINEBot\Exception\UnknownMessageTypeException;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+$path = __DIR__ . '/vendor/autoload.php';
+require_once $path;
 
-var_dump(new \LINE\LINEBot\HTTPClient\CurlHTTPClient('7xGXQTgIeNebLt9q7UtuY8TdPI6T8eAx1WxsHp5i38siySOMQrZ5wyc0A1xUFpLQ9s7DErBfFTdmeM3Gw73Clgxb4wN5uWpfGNgU68VkOFZMBu9ss2axWxrwMLfVR1WSBl7r02mgEKMO2pA59QVgKwdB04t89/1O/w1cDnyilFU='));
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('7xGXQTgIeNebLt9q7UtuY8TdPI6T8eAx1WxsHp5i38siySOMQrZ5wyc0A1xUFpLQ9s7DErBfFTdmeM3Gw73Clgxb4wN5uWpfGNgU68VkOFZMBu9ss2axWxrwMLfVR1WSBl7r02mgEKMO2pA59QVgKwdB04t89/1O/w1cDnyilFU=');
 
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd2a932fbcb794319177f57f78cdc9493']);
