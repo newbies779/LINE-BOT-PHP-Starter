bcb959073c6dc1e4cae1f425aed067232673c32e
ANUPONG_KNOT_PC\Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-27 11:46:34 +0700

    dsa

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $event['replyToken']
+				'text' => $event
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
