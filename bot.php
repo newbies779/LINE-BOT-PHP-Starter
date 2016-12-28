b059739a5b0432142503135cb55160a9efdd9d19
Anupong Chuen-Im <anupong.chuen@gmail.com>
2016-12-28 14:27:56 +0700

    ewqe

--- bot.php
+++ bot.php
@@ -9,39 +9,48 @@ echo 'running';
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
-	// foreach ($events['events'] as $event) {
-	// 	// Reply only when message sent is in 'text' format
-	// 	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-	// 		// Get text sent
-	// 		$text = $event['message']['text'];
-	// 		// Get replyToken
-	// 		$replyToken = $event['replyToken'];
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-	// 		// Build message to reply back
-	// 		$messages = [
-	// 			'type' => 'text',
-	// 			'text' => $event['source']['groupId']
-	// 		];
+			if(strpos($text, 'bot:giveID')){
+				if(isset($event['source']['userId'])){
+					$mes = $event['source']['userId'];
+				}else if(isset($event['source']['groupId'])){
+					$mes = $event['source']['groupId'];
+				}else{
+					$mes = 'No userId or groupId';
+				}
+			}	
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $mes
+			];
 
-	// 		// Make a POST Request to Messaging API to reply to sender
-	// 		$url = 'https://api.line.me/v2/bot/message/reply';
-	// 		$data = [
-	// 			'replyToken' => $replyToken,
-	// 			'messages' => [$messages],
-	// 		];
-	// 		$post = json_encode($data);
-	// 		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-	// 		$ch = curl_init($url);
-	// 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	// 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	// 		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	// 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	// 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	// 		$result = curl_exec($ch);
-	// 		curl_close($ch);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-	// 		echo $result . "\r\n";
-	// 	}
-	// }
+			echo $result . "\r\n";
+		}
+	}
 }
